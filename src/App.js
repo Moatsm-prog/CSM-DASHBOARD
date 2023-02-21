0f053a6f30de86842f3f826ca13392adb266eed0
ayatarek831 <64319299+ayatarek831@users.noreply.github.com>
2023-02-21 21:57:26 +0200

    edit app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
-
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Sidebar} from './components'
+import { useStateContext } from './contexts/ContextProvider';
+import './App.css'
 const App = () => {
+  const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
+
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem('colorMode');
+    const currentThemeMode = localStorage.getItem('themeMode');
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
+  }, []);
+
+
   return (
-    <div>App</div>
-  )
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
+    <BrowserRouter>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+        </div>
+        {activeMenu ? (
+          <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+            <Sidebar />
+          </div>
+        ) : (
+          <div className="w-0 dark:bg-secondary-dark-bg">
+            <Sidebar />
+          </div>
+        )}
+      </div>
+    </BrowserRouter>
+  </div>
+)
 }
 
 export default App
